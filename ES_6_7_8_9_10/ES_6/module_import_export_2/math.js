32cb0bede7524711ba6528ab4e3aa08dfae90549
Manoranjan Behera <behera.mranjan831@gmail.com>
2023-10-06 14:57:47 +0530

    Javascript updated

---
+++ ES_6_7_8_9_10/ES_6/module_import_export_2/math.js
@@ -0,0 +1,11 @@
+//default export
+export function add(a, b) {
+    return a + b;
+  }
+  
+  //named export
+  export function sub(a, b) {
+    return a - b;
+  }
+  
+  export let myName = "sanjay";
